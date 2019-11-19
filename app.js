991837534c377670f065d482b91b06e1876fedcc
Jon Davis <jonerikdavis@gmail.com>
2019-11-19 10:30:50 -0800

    removed auto format
    prettier issues

--- app.js
+++ app.js
@@ -18,9 +18,11 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.locals.moment = require('moment');
@@ -29,14 +31,14 @@ app.use('/appointments', appointments);
 app.use('/', appointments);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   console.error(err);
   res.status(err.status || 500);
   res.render('error', {
@@ -48,8 +50,10 @@ app.use(function (err, req, res, next) {
 if ((process.env.NODE_ENV || '').toLowerCase() !== 'test' && !!process.env.CI)
   scheduler.start();
 
-app.listen(cfg.port, function () {
-  console.log(`Starting sample-appointment-reminders at http://localhost:${cfg.port}`);
+app.listen(cfg.port, function() {
+  console.log(
+    `Starting sample-appointment-reminders at http://localhost:${cfg.port}`
+  );
 });
 
 module.exports = app;
